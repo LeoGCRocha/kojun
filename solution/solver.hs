3e78a7af39e867fc911b192f10526eeb0aae5a8c
Leonardo <gideonveloz@gmail.com>
2021-07-10 16:44:49 -0300

    pegar vetor

--- solution/solver.hs
+++ solution/solver.hs
@@ -14,7 +14,6 @@ type Coordenada = (Int, Int)
 -- campos vazios seram representados com 0
 type Ponto = (Int, Int)
 
-mat :: [[(Integer, Integer)]]
 mat = [[(0, 0), (0, 1), (4, 1), (0, 1), (2, 3), (0, 4)], [(0, 0), (0, 1), (3, 2), (0, 3), (0, 3), (0, 3)], [(1, 0), (4, 0), (0, 5), (4, 3), (0, 10), (0, 10)], [(0, 6), (5, 7), (0, 5), (0, 9), (0, 9), (2, 10)], [(0, 6), (0, 7), (0, 7), (0, 8), (3, 8), (0, 10)], [(6, 7), (2, 7), (0, 7), (2, 8), (0, 8), (5, 8)]]
 
 -- Metodos para os pontos
@@ -26,7 +25,16 @@ pegarValorPonto (_, x) = x
 pegarGrupoPonto :: Ponto -> Int
 pegarGrupoPonto (x,_) = x
 
+-- retorna um elemento na coordenada especificada, o segundo parametro de ponto define o tamanho do tabuleiro, ou seja o limite do jogo.
+-- (-1,-1) é o retorno padrão para erros
+pegarPontoCoordernada :: Coordenada -> [[Ponto]]-> Ponto 
+pegarPontoCoordernada (x,y) mat | ((x < 0) || (x > length mat)) = (-1, -1) -- array index out of range em x
+  | ((y < 0) || (y > length mat)) = (-1,-1) -- array index out of range em y
+  | otherwise = mat !! x !! y -- mat !! 0 !! 0 percorre o indice 0 , 0 por exemplo.
+
+main :: IO ()
 main = do
   -- Demonstração metodos, para testes
   print(pegarValorPonto (1,2))
   print(pegarGrupoPonto (1,2))
+  print(pegarPontoCoordernada (1,1) mat) -- teste para pegar um ponto
